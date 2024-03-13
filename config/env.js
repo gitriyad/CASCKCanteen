7dc53bf2daabefb5b19a202671b909beb18fba7c
Md. Nurul Huda Riyad <riyad.github@gmail.com>
2024-03-13 07:19:45 +0600

    t

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 process.env.secretKey =
   "Civil Aviation College(CASC) Canteen Management Software. Developed by Md. Nurul Huda Riyad(Lec, ICT)";
-//process.env.connectionString =
-// ("mongodb+srv://riyad303030:a2tQE1qXl9Aa0Woa@cluster0.5z6riau.mongodb.net/canteen");
 process.env.connectionString =
-  "mongodb://root:casck1234@103.148.15.38:27017/canteen";
+  "mongodb+srv://riyad303030:a2tQE1qXl9Aa0Woa@cluster0.5z6riau.mongodb.net/canteen";
+// process.env.connectionString =
+//   "mongodb://root:casck1234@103.148.15.38:27017/canteen";
 module.exports = process.env;
