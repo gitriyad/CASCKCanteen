f3539c4a632e4b5b85b3d80eca1da677f34363ca
Md. Nurul Huda Riyad <riyad.github@gmail.com>
2024-03-15 13:23:54 +0600

    checkAUTO

--- config/env.js
+++ config/env.js
@@ -4,6 +4,7 @@ process.env.secretKey =
 //   "mongodb+srv://riyad303030:a2tQE1qXl9Aa0Woa@cluster0.5z6riau.mongodb.net/canteen";
 //process.env.connectionString = "mongodb://127.0.0.1:27017/canteen";
 // process.env.connectionString = "mongodb://103.148.15.38:27017/canteen";
+//this is the secure mongoConnectionString
 process.env.connectionString =
   "mongodb://casckCanteenAdminUser1:casck1234canteenAdminUser1@103.148.15.38:31716/canteen";
 module.exports = process.env;
